1e6df5388b836a61b3e19fdeef3237664526e875
SteamDonkey74 <arobins@spiretech.com>
2013-02-27 22:38:26 -0800

    Create lsd.c
    C program that, when compiled and launched, displays ASCII representation of molecule for LSD.
    
    Doubtlessly, there are simpler ways to code the same thing. I have simply adapted the iconic hello.c program from Ritchie/Kernighan, as I am re-learning my C programming skills.

---
+++ lsd.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+main()
+{
+printf(" # ASCII rendition of Lysergic Acid Diethylamide\n");
+printf(" # (9,10-didehydro-N,N-diethyl-6-methyl-8b-ergoline-8-carboxamide)\n");
+printf(" # C20 H25 N3 O\n");
+printf(" # ASCII rendition credited to Ben Norwood\n");
+printf("\n");
+printf("\n");
+printf("                     H\n");
+printf("                     H\n");
+printf("                     |\n");
+printf("               H  H  C--H\n");
+printf("                \\`.|,'|\n");
+printf("                  C  H  H\n");
+printf("                  |     |\n");
+printf("             O    N  H  C\n");
+printf("             \\\\\\\\ ,' \\`.|,'|\\`.\n");
+printf("               C     C  H  H\n");
+printf("               |     |\n");
+printf("            H--C     H\n");
+printf("             ,' \\`.\n");
+printf("      H  H--C  H--C--H\n");
+printf("      |     ||    |\n");
+printf("H     C     C     N  H  H\n");
+printf(" \\`. ,' \\`. ,' \\`. ,' \\`.|,'\n");
+printf("   C  _  C  H  C     C\n");
+printf("   | (_) |   \\`.|     |\n");
+printf("   C     C     C     H\n");
+printf(" ,' \\`. ,' \\`. ,' \\`.\n");
+printf("H     C     C     H\n");
+printf("      |    ||\n");
+printf("      N-----C\n");
+printf("      |     |\n");
+printf("      H     H\n");
+
+}
